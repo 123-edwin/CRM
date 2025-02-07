9b7ef42eadf691ca6a83e81096a6a4a6d612f116
Isai-bot777 <erios8@ucol.mx>
2025-02-07 16:29:37 -0600

    cambios router y pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "http://Isai-bot777.github.io/CRM"
+  base: "http://123-edwin.github.io/CRM"
 })
