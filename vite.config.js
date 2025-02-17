c48914e6faa3f866bc6e9efe6fc4e9cc7a4f5643
Isai-bot777 <erios8@ucol.mx>
2025-02-17 15:53:09 -0600

    alias

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "http://123-edwin.github.io/CRM"
+  base: "http://123-edwin.github.io/CRM",
+  resolve: {
+    alias: {
+      '@c': path.resolve(__dirname, './src/components')
+    }
+  }
 })
