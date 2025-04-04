b1a16d22320fe9d81634884df4b41f59c33b56cb
Isai-bot777 <erios8@ucol.mx>
2025-04-03 19:35:12 -0600

    Factura

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import {Client} from '@p/Client/Client'
 import {NavBar} from '@c/navbar/NavBar'
-import { MultiSectionForm } from './pages/Client/MultiSectionForm'
+import { MultiSectionForm } from '@p/Client/MultiSectionForm'
+import BillForm from '@p/Bill/BillForm'
 
 function App() {
   
@@ -16,6 +17,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Client/>} />
         <Route path="/Form" element={<MultiSectionForm/>} />
+        <Route path="/factura" element={<BillForm/>} />
       </Routes>
     </BrowserRouter>
 
